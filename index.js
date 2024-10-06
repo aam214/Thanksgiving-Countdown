40f35b77b543cfc9a3977f72ae98058a7956a615
Ariana <152944814+aam214@users.noreply.github.com>
2024-10-06 12:32:46 -0500

    Added Mat.floor to minutesNow and secondsNow. Added setTimeout method. Added if/else statements for display of 0.

--- index.js
+++ index.js
@@ -13,4 +13,24 @@ const seconds = 1000;
 const minutes = seconds * 60;
 const hours = minutes * 60;
 const days = hours * 24;
+
+let daysNow = Math.floor(gap / days);
+let hoursNow = Math.floor((gap % days) / hours);
+let minutesNow = Math.floor((gap % hours) / minutes);
+let secondsNow = Math.floor((gap % minutes) / seconds);
+
+hoursNow= hoursNow < 10 ? '0' + hoursNow : hoursNow;
+minutesNow = minutesNow < 10 ? '0' + minutesNow : minutesNow;
+secondsNow = secondsNow < 10 ? '0' + secondsNow : secondsNow;
+
+daysElement.innerHTML= daysNow;
+hoursElement.innerHTML = hoursNow;
+
+minutesElement.innerHTML = minutesNow;
+secondsElement.innerHTML = secondsNow;
+
+setTimeout(() => {
+  countDown();
+}, 1000);
 }
+

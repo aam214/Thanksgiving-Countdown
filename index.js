7fe368897e95f94ac5e7ee73ca1b30318970db13
Ariana <152944814+aam214@users.noreply.github.com>
2024-10-06 13:48:40 -0500

    renamed days variable to day. Added semicolons where needed for correct syntax.

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const hoursElement = document.getElementById("hours");
 const minutesElement = document.getElementById("minutes");
 const secondsElement = document.getElementById("seconds");
 
-const ThanksgivingDate = new Date("Nov 28, 2024").getTime()
-countDown()
+const ThanksgivingDate = new Date("Nov 28, 2024").getTime();
+countDown();
 
 function countDown() {
 const currentTime = new Date().getTime();
@@ -12,10 +12,10 @@ const gap = ThanksgivingDate - currentTime;
 const seconds = 1000;
 const minutes = seconds * 60;
 const hours = minutes * 60;
-const days = hours * 24;
+const day = hours * 24;
 
-let daysNow = Math.floor(gap / days);
-let hoursNow = Math.floor((gap % days) / hours);
+let daysNow = Math.floor(gap / day);
+let hoursNow = Math.floor((gap % day) / hours);
 let minutesNow = Math.floor((gap % hours) / minutes);
 let secondsNow = Math.floor((gap % minutes) / seconds);
 
